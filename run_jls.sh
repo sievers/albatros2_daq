7f81adb1e33e73fbf15d779c6d973e5af9bba830
jonathan <jonathan.sievers@gmail.com>
2019-04-07 20:23:39 +0000

    added automatic MAC address detection

--- run_jls.sh
+++ run_jls.sh
@@ -7,6 +7,8 @@ max_fpga_attempts=10
 attempts=0
 success=0
 
+mac=`./find_mac.py`
+
 
 while [ $attempts -lt $max_fpga_attempts -a  $success -eq 0 ]
 do
@@ -14,7 +16,9 @@ do
     ((attempts++))
     
     #python albatros2_daq_jls.py 127.0.0.1:7147 -f /home/pi/firmware/quad_input_poco_gbe_2019-03-23_1523.fpg -c internal -a 393216 -F 65535 -b $bytes -d "192.168.2.200:4321" -m 0xb827eb6091e5  -l /home/pi/logs/ -Q "$freq"
-    python albatros2_daq_jls.py 127.0.0.1:7147 -f /home/pi/firmware/quad_input_poco_gbe_2019-03-23_1523.fpg -c internal -a 393216 -F 65535 -b $bytes -d "192.168.2.200:4321" -m 0xb827eba3d4b0  -l /home/pi/logs/ -Q "$freq" 
+    #python albatros2_daq_jls.py 127.0.0.1:7147 -f /home/pi/firmware/quad_input_poco_gbe_2019-03-23_1523.fpg -c internal -a 393216 -F 65535 -b $bytes -d "192.168.2.200:4321" -m 0xb827eb8e5bcd  -l /home/pi/logs/ -Q "$freq"
+
+    python albatros2_daq_jls.py 127.0.0.1:7147 -f /home/pi/firmware/quad_input_poco_gbe_2019-03-23_1523.fpg -c internal -a 393216 -F 65535 -b $bytes -d "192.168.2.200:4321" -m $mac  -l /home/pi/logs/ -Q "$freq" 
     #check the return value from the python script, returned via sys.exit()
     retval=$?  
     if [ $retval -eq 0 ]; then
