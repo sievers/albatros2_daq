c3495a504b5daf86fc2cc80546dc390d39aeda9f
nivekg <nivekghazi94@gmail.com>
2019-11-04 11:06:24 +0000

    Now a high level wrapper for all firmware functions
    * All failure checking code moved to config_fpga.py
    * There are now specific functions for controlling different parts
      of the firmware

--- daq_2020/albatrosdigitizer.py
+++ daq_2020/albatrosdigitizer.py
@@ -5,127 +5,48 @@ import struct
 import time
 import math
 import numpy
+import logging
+
+logger=logging.getLogger(__name__)
 
 def str2ip(ip_str):
     octets=map(int, ip_str.split("."))
     ip=(octets[0]<<24)+(octets[1]<<16)+(octets[2]<<8)+(octets[3])
     return ip
 
-def float2fixed(value, binary_point):
-    return value*2**binary_point
-
-def fixed2float(value, binary_point):
-    return value/2.**binary_point
-
 class AlbatrosDigitizer:
-    def __init__(self, snap_ip, snap_port, logger):
-        self.logger=logger
+    def __init__(self, snap_ip, snap_port):
         self.fpga=casperfpga.CasperFpga(host=snap_ip, port=snap_port, transport=casperfpga.KatcpTransport)
-        self.logger.info("Connected to SNAP Board at %s:%s"%(snap_ip, snap_port))
         if self.fpga.is_running():
-            self.logger.info("Fpga is already programmed")
+            logger.info("Fpga is already programmed")
 	    self.fpga.get_system_information()
         else:
-            self.logger.info("Fpga not programmed")
+            logger.info("Fpga not programmed")
 
-    def initialise_fpga(self, fpg_file, fftshift, acclen, bits, spec_per_packet, bytes_per_spectrum, prog_tries=3):
-        self.logger.info("Initialising SNAP Board")
-        for i in range(prog_tries):
-            if self.fpga.upload_to_ram_and_program(fpg_file):
-                self.logger.info("Fpga programmed sucessfully after %d attempt/s"%(i+1))
-                break
-            elif i<prog_tries-1:
-                self.logger.error("Failed to program. Retrying!!!")
-            else:
-                self.logger.critical("Failed to program after "+str(prog_tries)+" tries.")
-                return False
-        self.logger.info("FPGA clock: %f"%self.fpga.estimate_fpga_clock())
-        self.logger.info("Setting FFT shift")
-        self.fpga.registers.pfb_fft_shift.write_int(fftshift)
-        self.logger.info("Setting accumulation length")
-        self.fpga.registers.acc_len.write_int(acclen)
-        self.logger.info("Setting ouput bits")
-        self.fpga.registers.output_mux_sel.write_int(int(math.log(bits,2)))
-        self.logger.info("Reseting packetiser")
-        self.fpga.registers.output_rst.write_int(1)
-        self.fpga.registers.gbe_output_en.write_int(0)
-        self.logger.info("Setting spectra per packet")
-        self.fpga.registers.output_ctrl_spec_per_pkt.write_int(spec_per_packet)
-        self.logger.info("Setting bytes per spectra")
-        self.fpga.registers.output_ctrl_bytes_per_spec.write_int(bytes_per_spectrum)
-        time.sleep(0.5)
-        self.logger.info("Enabling 1 GbE output")
-        self.fpga.registers.gbe_output_en.write_int(1)
-        self.fpga.registers.output_rst.write_int(0)
-        time.sleep(0.5)
-        self.logger.info("Initialisation complete")
-        return True
-
-    def initialise_adcs(self, ref_clock, adc_digital_gain, adc_tries=3):
-        snap_adc=casperfpga.snapadc.SNAPADC(self.fpga, ref=ref_clock, resolution=8, cs=0xff)
-        for i in range(adc_tries):
-            if snap_adc.init(samplingRate=250, numChannel=4)==0:
-                for j in range(3):
-                    snap_adc.selectADC(j)
-                    snap_adc.adc.selectInput([1,2,3,4])
-                    self.logger.info("ADC's initialised after %d attempts/s"%(i+1))
-                    break
-            elif i<adc_tries-1:
-                self.logger.error("ADC initialisation failed. Retrying!!!")
-            else:
-                self.logger.critical("ADC initialisation failed after "+str(adc_tries)+" tries. Exiting!!!")
-                return False
-        self.logger.info("Setting ADC digital gain")
-        snap_adc.adc.cGain([adc_digital_gain, adc_digital_gain, adc_digital_gain, adc_digital_gain])
-        self.logger.info("Powering down ADC's 2 and 3")
-        snap_adc_pd=casperfpga.snapadc.SNAPADC(self.fpga, ref=ref_clock, resolution=8, cs=0x06)
-        snap_adc_pd.adc.powerDown()
-        return None
-
-    def initialise_gbe(self, dest_ip, dest_port, dest_mac):
-        self.logger.info("Setting destination IP address and port")
-        self.fpga.registers.gbe_output_dest_ip.write_int(str2ip(dest_ip))
-        self.fpga.registers.gbe_output_dest_port.write_int(dest_port)
-        self.logger.info("Setting destination mac address")
-        mac=int(dest_mac, 16)
-        for i in range(256):
-            self.fpga.write("gbe_output_one_gbe", struct.pack(">Q", mac), offset=0x3000+8*i)
-        return None
-
-    def sync(self):
-        self.logger.info("Resetting counters and syncing")
-        self.fpga.registers.cnt_rst.write_int(0)
-        self.fpga.registers.sw_sync.write_int(0)
-        self.fpga.registers.sw_sync.write_int(1)
-        self.fpga.registers.sw_sync.write_int(0)
-        self.fpga.registers.cnt_rst.write_int(1)
-        self.fpga.registers.cnt_rst.write_int(0)
-        return None
-
-    def set_channel_order(self, channels, bits):
-        channel_map=""
-        if (bits==1):
-            channel_map="reorder_4_to_8_reorder_map1"
-        elif (bits==2):
-            channel_map="reorder_8_to_8_reorder1_map1"
-        else:
-            channel_map="reorder_16_to_8_reorder2_map1"
-        self.fpga.write(channel_map, channels.astype(">H").tostring(), offset=0)
-        return True
-
-    def set_channel_coeffs(self, coeffs, bits):
-        if (bits==1):
-            self.logger.info("In one bit mode. No need to write coeffs")
+    def initialise_fpga(self, fpg_file):
+        logger.info("Programming FPGA")
+        if self.fpga.upload_to_ram_and_program(fpg_file):
             return True
-        coeffs_bram_name=""
-        if (bits==2):
-            coeffs_bram_name="two_bit_quant_coeffs"
-            self.logger.info("Setting two bit coeffs")
-        if (bits==4):
-            coeffs_bram_name="four_bit_quant_coeffs"
-            self.logger.info("Setting four bit coeffs")
-        self.fpga.write(coeffs_bram_name, coeffs.tostring(), offset=0)
-        return True 
+        else:
+            return False
+
+    def initialise_adcs(self, ref_clock, adc_digital_gain, powerdown):
+        snap_adc=casperfpga.snapadc.SNAPADC(self.fpga, ref=ref_clock, resolution=8, cs=0xff)
+        logger.info("Setting up ADC's")
+        if snap_adc.init(samplingRate=250, numChannel=4)==0:
+            for j in range(3):
+                snap_adc.selectADC(j)
+                snap_adc.adc.selectInput([1,2,3,4])
+            logger.info("FPGA clock: %.2f"%(self.fpga.estimate_fpga_clock()))
+            logger.info("Setting ADC digital gain")
+            snap_adc.adc.cGain([adc_digital_gain, adc_digital_gain, adc_digital_gain, adc_digital_gain])
+            if powerdown:
+                logger.info("Powering down ADC's 2 and 3")
+                snap_adc_pd=casperfpga.snapadc.SNAPADC(self.fpga, ref=ref_clock, resolution=8, cs=0x06)
+                snap_adc_pd.adc.powerDown()
+            return True
+        else:
+            return None
 
     def get_adc_stats(self):
         adc_stats={}
@@ -138,24 +59,86 @@ class AlbatrosDigitizer:
             bits_used=numpy.log2(rms)
             adc_stats["adc%d"%(i)]={"raw":data, "mean":mean, "rms":rms, "bits_used":bits_used}
         return adc_stats
+    
+    def initialise_pfb(self, fft_shift):
+        self.fpga.registers.fft_shift.write_int(fft_shift)
+        return None
 
+    def initialise_xcorr(self, acc_len):
+        self.fpga.registers.xcorr_acc_len.write_int(acc_len)
+        return None
+
+    def initialise_output_control(self, bits, chans, coeffs):
+        self.fpga.registers.output_control_select.write_int(int(numpy.log2(bits)))
+        self._set_channel_order(chans, bits)
+        self._set_channel_coeffs(coeffs, bits)
+        return None
+
+    def _set_channel_order(self, channels, bits):
+        channel_map=""
+        if (bits==1):
+            channel_map="output_control_one_bit_reorder_map1"
+        elif (bits==2):
+            channel_map="output_control_two_bit_reorder_map1"
+        else:
+            channel_map="output_control_four_bit_reorder_map1"
+        self.fpga.write(channel_map, channels.astype(">H").tostring(), offset=0)
+        return True
+
+    def _set_channel_coeffs(self, coeffs, bits):
+        if (bits==1):
+            logger.info("In one bit mode. No need to write coeffs")
+            return None
+        coeffs_bram_name=""
+        if (bits==2):
+            coeffs_bram_name="two_bit_quant_coeffs"
+            logger.info("Setting two bit coeffs")
+        if (bits==4):
+            coeffs_bram_name="four_bit_quant_coeffs"
+            logger.info("Setting four bit coeffs")
+        self.fpga.write(coeffs_bram_name, coeffs.tostring(), offset=0)
+        return None
+    
+    def initialise_packetiser(self, spec_per_packet, bytes_per_spectrum):
+        self.fpga.registers.packetiser_spectra_per_packet.write_int(spec_per_packet)
+        self.fpga.registers.packetiser_bytes_per_spectrum.write_int(bytes_per_spectrum)
+        return None
+    
+    def initialise_gbe(self, dest_ip, dest_port, dest_mac):
+        logger.info("Setting destination IP address and port")
+        self.fpga.registers.destination_ip.write_int(str2ip(dest_ip))
+        self.fpga.registers.destination_port.write_int(dest_port)
+        logger.info("Setting destination mac address")
+        self.fpga.gbes.one_gbe.set_arp_table([dest_mac]*256)
+        return None
+
+    def gbe_enable(self):
+        self.fpga.registers.control.write(gbe_en=1)
+
+    def gbe_disable(self):
+        self.fpga.registers.control.write(gbe_en=0)
+    
+    def sync(self):
+        logger.info("Resetting counters and syncing")
+        self.fpga.registers.control.write(cnt_rst=0)
+        self.fpga.registers.control.write(sw_sync=0)
+        self.fpga.registers.control.write(sw_sync=1)
+        self.fpga.registers.control.write(sw_sync=0)
+        self.fpga.registers.control.write(cnt_rst=1)
+        self.fpga.registers.control.write(cnt_rst=0)
+        return None
+
+    def check_pfb_overflow(self):
+        return self.fpga.registers.pfb_fft_of.read_uint()
+
+    def check_gbe_overflow(self):
+        return self.fpga.registers.tx_of_cnt.read_uint()
+                              
     def read_register(self, reg):
         return self.fpga.registers[reg].read_uint()
 
-    def read_bram(self, bram, nbytes):
-        return self.fpga.read(bram, nbytes)
-
-    def read_pols(self, pols, struct_format):
-        pols_dict = {}
-        for pol in pols:
-            pols_dict[pol] = numpy.array(struct.unpack(struct_format, self.fpga.read(pol, 2048*8)), dtype="int64")
-        return pols_dict
-
-    def read_registers(self, regs):
-        reg_dict = {}
-        for r in regs:
-            reg_dict[r] = numpy.array(self.fpga.registers[r].read_uint())
-        return reg_dict
+    def read_bram(self, bram):
+        return self.fpga.sbrams[bram].read_raw()
 
     def get_fpga_temperature(self):
         TEMP_OFFSET = 0x0
