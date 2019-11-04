d2af9d57c63c3ca2fb53770b7cc62bdb5ca8115a
nivekg <nivekghazi94@gmail.com>
2019-11-04 14:03:48 +0000

    Moved to use yaml config file

--- daq_2020/dump_baseband.py
+++ daq_2020/dump_baseband.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python
 import argparse
-import ConfigParser
+import yaml
 import socket
 import logging
 import time
 import os
-import albatros_daq_utils
+import utils
 import datetime
 import math
 import trimble_utils
 import numpy
 import subprocess
+import smbus
+import ds3231
+import sys
+import traceback
+import psutil
 
-def write_header(file_object, chans, spec_per_packet, bytes_per_packet, bits):
+def write_header(file_object, chans, spec_per_packet, bytes_per_packet, bits, logger):
     header_bytes=8*10+8*len(chans)
     have_trimble=False
     gps_time=trimble_utils.get_gps_time_trimble()
     if gps_time is None:
         logger.info("File timestamp coming for RPi Clock. This is unrealiable")
-        gps_time_rpi=albatros_daq_utils.gps_time_from_rtc()
+        gps_time_rpi=utils.gps_time_from_rtc()
         print(gps_time_rpi)
         gps_time={}
         gps_time['week']=gps_time_rpi["week"]
         gps_time['seconds']=gps_time_rpi["seconds"]
     else:
-        print 'ps time is now ',gps_time['week'],gps_time['seconds']
+        logger.debug('ps time is now ',gps_time['week'],gps_time['seconds'])
         have_trimble=True
     file_header=numpy.asarray([header_bytes, bytes_per_packet, len(chans), spec_per_packet, bits, have_trimble], dtype='>Q')
     file_header.tofile(file_object)
@@ -39,115 +44,183 @@ def write_header(file_object, chans, spec_per_packet, bytes_per_packet, bits):
         latlon['lon']=0
         latlon['elev']=0
     else:
-        print 'lat/lon/elev are ',latlon['lat'],latlon['lon'],latlon['elev']
+        logger.debug('lat/lon/elev are ',latlon['lat'],latlon['lon'],latlon['elev'])
     latlon=numpy.asarray([latlon['lat'],latlon['lon'],latlon['elev']],dtype='>d')
     latlon.tofile(file_object)
     return None
 
-def spin_down_drive(drive_block, mount_point):
-    pass
-
-def spin_up_drive(drive_block, partition, mount_point):
-    pass
-
 if __name__=="__main__":
-    parser=argparse.ArgumentParser(description="Script to save baseband")
-    parser.add_argument("-c", "--configfile", type=str, default="config.ini", help="Config file with all the parameters")
+    parser=argparse.ArgumentParser()
+    parser.add_argument("-c", "--configfile", type=str, default="config.yaml", help="yaml file with parameters for dump baseband")
     args=parser.parse_args()
 
-    config_file=ConfigParser.SafeConfigParser()
-    config_file.read(args.configfile)
+    parameters=None
+    with open(args.configfile, "r") as cf:
+        parameters=yaml.load(cf.read(), yaml.FullLoader)
 
-    logger=logging.getLogger("albatros2_dump_baseband")
-    logger.setLevel(logging.INFO)
-    baseband_log_dir=config_file.get("albatros2", "dump_baseband_log_directory")
-    if not os.path.isdir(baseband_log_dir):
-        os.mkdir(baseband_log_dir)
-    file_logger=logging.FileHandler(baseband_log_dir+"albatros_dump_baseband_"+datetime.datetime.now().strftime("%d%m%Y_%H%M%S")+".log")
-    file_format=logging.Formatter("%(asctime)s %(name)s %(message)s", "%d-%m-%Y %H:%M:%S")
-    file_logger.setFormatter(file_format)
-    file_logger.setLevel(logging.INFO)
-    logger.addHandler(file_logger)
-    logger.info("########################################################################################")
-    dest_ip=config_file.get("albatros2", "destination_ip")
-    logger.info("# (1) Destination ip: %s"%(dest_ip))
-    dest_port=config_file.getint("albatros2", "destination_port")
-    logger.info("# (2) Destination port: %d"%(dest_port))
-    channels=config_file.get("albatros2", "channels")
-    logger.info("# (3) Channels: %s"%(channels))
-    bits=config_file.getint("albatros2", "bits")
-    logger.info("# (4) Baseband bits: %d"%(bits))
-    max_bytes_per_packet=config_file.getint("albatros2", "max_bytes_per_packet")
-    logger.info("# (5) Max bytes per packet: %d"%(max_bytes_per_packet))
-    drives_full=config_file.getboolean("albatros2", "drives_full")
-    logger.info("# (6) Drives full: %r"%(drives_full))
-    drive_safety=config_file.getfloat("albatros2", "drive_safety")
-    logger.info("# (7) Drive safety percentage: %.2f"%(drive_safety))
-    file_size=config_file.getfloat("albatros2", "file_size")
-    logger.info("# (8) File size: %f"%(file_size))
-    dump_baseband_directory_name=config_file.get("albatros2", "dump_baseband_directory_name")
-    logger.info("# (9) Baseband directory name: %s"%(dump_baseband_directory_name))
-    logger.info("# (10) Log directory: %s"%(baseband_log_dir))
-    logger.info("########################################################################################")
-
-    sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  
-    try:
-        sock.bind((dest_ip, dest_port))
-        logger.info("Connected to %s:%d"%(dest_ip, dest_port))
-    except:
-        logger.error("Cannot bind to %s:%d"%(dest_ip, dest_port))
-
-    chans=albatros_daq_utils.get_channels_from_str(channels, bits)
-    spec_per_packet=albatros_daq_utils.get_nspec(chans, max_nbyte=max_bytes_per_packet)
-    bytes_per_spectrum=chans.shape[0]
-    bytes_per_packet=bytes_per_spectrum*spec_per_packet+4 #the 4 extra bytes is for the spectrum number
-    packet=bytearray(bytes_per_packet)
-    num_of_packets_per_file=int(math.floor(file_size*1.0e9/bytes_per_packet))
-    
-    drives=albatros_daq_utils.list_drives_to_write_too()
-    logger.info("Found these drive/s")
-    logger.info("%-17s %-17s %-17s %-17s %-5s%% %-s"%("Device", "Total", "Used", "Free", "Use ", "Mount"))
-    for drive in drives:
-        logger.info("%-17s %-17s %-17s %-17s %-5s%% %-s"%(drive["Device"], drive["Blocks"], drive["Used"], drive["Available"], drive["Use%"], drive["Mounted on"]))
-    
-    reads_for_many_packets=50000
-    time_for_many_packets=(2048*2/250e6)*spec_per_packet*reads_for_many_packets
-    
-    while not drives_full:
-        if len(drives)==0:
-            logger.info("No drives found. Check if drives are connected and mounted")
-        else:
-            for drive in drives:
-                drive_path=drive["Mounted on"]
-                number_of_files=albatros_daq_utils.num_files_can_write(drive_path, drive_safety, file_size)
-                if number_of_files>0:
-                    write_path=drive_path+"/"+dump_baseband_directory_name
-                    if not os.path.isdir(write_path):
-                        os.mkdir(write_path)
-                    for i in range(number_of_files):
-                        directory_time=str(int(time.time()))[:5]
-                        if not os.path.isdir(write_path+"/"+directory_time):
-                            os.mkdir(write_path+"/"+directory_time)
-                        file_time=int(time.time())
-                        file_path=write_path+"/"+directory_time+"/"+str(file_time)+".raw"
-                        baseband_file=open(file_path, "w")
-                        write_header(baseband_file, chans, spec_per_packet, bytes_per_packet, bits)
-                        logger.info("Writing file "+str(i+1)+" of "+str(number_of_files)+" to "+file_path)
-                        time_for_many_reads=time.time()
-                        num_of_reads=0
-                        for j in range(num_of_packets_per_file):
-                            sock.recvfrom_into(packet, bytes_per_packet)
-                            baseband_file.write(packet)
-                            num_of_reads=num_of_reads+1
-                            if num_of_reads==reads_for_many_packets:
-                                time_after_many_reads=time.time()
-                                print("Data size is ", len(packet), type(packet), time_after_many_reads-time_for_many_reads, time_for_many_packets)
-                                time_for_many_reads=time_after_many_reads
-                                num_of_reads=0
-                        baseband_file.close()
-            drives_full=True
+    log_level=parameters["log_level"]
+    log_directory=parameters["log_directory"]
         
-    logger.info("All drives full. Not saving baseband. Pausing here!!!")
-    while True:
-        pass
+    if not os.path.isdir(log_directory):
+        os.makedirs(log_directory)
+
+    logging.basicConfig(level=log_level,
+                        format='%(asctime)s %(name)-20s %(levelname)-8s %(message)s',
+                        datefmt="%d-%m-%Y %H:%M:%S",
+                        filename=log_directory+"/dump_baseband_"+datetime.datetime.now().strftime("%d%m%Y_%H%M%S")+".log",
+                        filemode='w')
+
+    logger=logging.getLogger("dump_baseband")
+
+    i2c_bus=smbus.SMBus(1)
+    rtc=ds3231.DS3231(i2c_bus, 0x68)
+
+    snap_board=parameters["snap-board"]
+    firmware=parameters["firmware"]
+    synthesizer=parameters["synthesizer"]
+    adcs=parameters["adcs"]
+    pfb=parameters["pfb"]
+    test_vector_generator=parameters["test-vector-generator"]
+    xcorr=parameters["xcorr"]
+    output_control=parameters["output-control"]
+    packetiser=parameters["packetiser"]
+    baseband=parameters["baseband"]
+    mount_directory=parameters["mount_directory"]
+    harddrives=parameters["hard-drives"]
+    spectra=parameters["spectra"]
+    ethernet=parameters["ethernet"]
+
+    chans=utils.get_channels_from_str(output_control["channels"], output_control["bits"])
+    spectra_per_packet=utils.get_nspec(chans, max_nbyte=packetiser["max_bytes_per_packet"])
+    bytes_per_spectrum=chans.shape[0]
+    bytes_per_packet=spectra_per_packet*bytes_per_spectrum+4
+    coeffs=utils.get_coeffs_from_str(output_control["channel_coeffs"])
+
+    logger.info("############### config-parameters ###############")
+    logger.info("SNAP-BOARD:")
+    logger.info("\tip: %s"%(snap_board["ip"]))
+    logger.info("\tport: %d"%(snap_board["port"]))
+    logger.info("FIRMWARE:")
+    logger.info("\tfpg_file: %s"%(firmware["fpg_file"]))
+    logger.info("SYNTHESIZER:")
+    logger.info("\tinput: %s"%(synthesizer["input"]))
+    logger.info("\tfrequency: %d"%(synthesizer["frequency"]))
+    logger.info("ADCS:")
+    logger.info("\tadc digital gain: %d"%(adcs["adc_digital_gain"]))
+    logger.info("\tpowerdown unused adcs: %s"%(adcs["powerdown_unused_adcs"]))
+    logger.info("PFB:")
+    logger.info("\tfft shift: %s"%(hex(pfb["fftshift"])[:-1]))
+    logger.info("TEST-VECTOR-GENERATOR:")
+    logger.info("\tenable: %s"%(str(test_vector_generator["enable"])))
+    logger.info("\tdata: %s"%(test_vector_generator["data"]))
+    logger.info("XCORR:")
+    logger.info("\taccumulation length: %d"%(xcorr["accumulation_length"]))
+    logger.info("OUTPUT CONTROL:")
+    logger.info("\tbits: %d"%(output_control["bits"]))
+    logger.info("\tchannels: %s"%(output_control["channels"]))
+    logger.info("\tchannel coeffs: %s"%(output_control["channel_coeffs"]))
+    logger.info("PACKETISER:")
+    logger.info("\tmax bytes per packet: %d"%(packetiser["max_bytes_per_packet"]))
+    logger.info("ETHERNET:")
+    logger.info("\tlocal ip: %s"%(ethernet["local_ip"]))
+    logger.info("\tlocal port: %d"%(ethernet["local_port"]))
+    logger.info("\tlocal mac: %s"%(hex(ethernet["local_mac"])[:-1]))
+    logger.info("\tdestination ip: %s"%(ethernet["destination_ip"]))
+    logger.info("\tdestination port: %d"%(ethernet["destination_port"]))
+    destination_mac=None
+    eth0=psutil.net_if_addrs()["eth0"]
+    for link in eth0:
+        if link.family == psutil.AF_LINK:
+            destination_mac=int("0x"+link.address.replace(":", ""), 16)
+    logger.info("\tdestination mac: %s"%(hex(destination_mac)[:-1]))
+    logger.info("BASEBAND:")
+    logger.info("\tdrive safety: %.2f"%(baseband["drive_safety"]))
+    logger.info("\tfile size: %.2f"%(baseband["file_size"]))
+    logger.info("\tdirectory name: %s"%(baseband["directory_name"]))
+    logger.info("MOUNT-DIRECTORY:")
+    logger.info("\tmount directory: %s"%(mount_directory))
+    logger.info("HARD-DRIVES:")
+    for drive in harddrives:
+        drive_tag=drive.keys()[0]
+        logger.info("\t%s:"%(drive_tag))
+        logger.info("\t\tlabel: %s"%(drive[drive_tag]["label"]))
+        logger.info("\t\tpm: %s"%(drive[drive_tag]["pm"]))
+    logger.info("SPECTRA:")
+    logger.info("\tdiff: %s"%(spectra["diff"]))
+    logger.info("\tcompress: %s"%(spectra["compress"]))
+    logger.info("\tfile size: %d"%(spectra["file_size"]))
+    logger.info("\tdirectory name: %s"%(spectra["directory_name"]))
+    logger.info("\tdata directory: %s"%(spectra["data_directory"]))
+    logger.info("CALCULATED PARAMETERS:")
+    logger.info("\tspectra per packet: %d"%(spectra_per_packet))
+    logger.info("\tbytes per spectrum: %d"%(bytes_per_spectrum))
+    logger.info("\tbytes per packet: %d"%(bytes_per_packet))
+    logger.info("###################### end ######################")
+    
+    sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    try:
+        sock.bind((ethernet["destination_ip"], ethernet["destination_port"]))
+        ip, port=sock.getsockname()
+        logger.info("Connected to %s:%d"%(ip, port))
+        logger.info("Setting socket timeout to 10 seconds")
+        sock.settimeout(10.0)
+    except:
+        logger.fatal("Cannot bind to %s:%d"%(ethernet["destination_ip"], ethernet["destination_port"]))
+        exit(1)
+
+    packet=bytearray(bytes_per_packet)
+
+    num_of_packets_per_file=int(math.floor(baseband["file_size"]*1.024e6/bytes_per_packet))
+
+    reads_for_many_packets=50000
+    time_for_many_packets=(2048*2/250e6)*spectra_per_packet*reads_for_many_packets
+
+    if not os.path.isdir(mount_directory):
+        os.makedirs(mount_directory)
+
+    try:
+        for drive in harddrives:
+            drive_tag=drive.keys()[0]
+            logger.info("Mounting %s:%s to %s"%(drive_tag, drive[drive_tag]["label"], mount_directory))
+            process_mount=subprocess.Popen(["sudo", "mount", "-L", drive[drive_tag]["label"], mount_directory]).wait()
+            number_of_files=utils.num_files_can_write(mount_directory, baseband["drive_safety"], baseband["file_size"])
+            logger.debug("Number of files: %d"%(number_of_files))
+            if number_of_files>0:
+                write_path=mount_directory+"/"+baseband["directory_name"]
+                for i in range(number_of_files):
+                    directory_time=str(int(time.time()))[:5]
+                    if not os.path.isdir(write_path+"/"+directory_time):
+                        os.makedirs(write_path+"/"+directory_time)
+                    file_time=int(time.time())
+                    file_path=write_path+"/"+directory_time+"/"+str(file_time)+".raw"
+                    baseband_file=open(file_path, "w")
+                    write_header(baseband_file, chans, spectra_per_packet, bytes_per_packet,
+                                 output_control["bits"], logger)
+                    logger.info("Writing file "+str(i+1)+" of "+str(number_of_files)+" to "+file_path)
+                    time_for_many_reads=time.time()
+                    num_of_reads=0
+                    for j in range(num_of_packets_per_file):
+                        sock.recvfrom_into(packet, bytes_per_packet)
+                        baseband_file.write(packet)
+                        num_of_reads=num_of_reads+1
+                        if num_of_reads==reads_for_many_packets:
+                            time_after_many_reads=time.time()
+                            logger.debug("Data size is %s, %s, %d, %.4f"%(len(packet),
+                                                                          type(packet),
+                                                                          time_after_many_reads-time_for_many_reads,
+                                                                          time_for_many_packets))
+                            time_for_many_reads=time_after_many_reads
+                            num_of_reads=0
+                    baseband_file.close()
+            logger.info("Un-mounting %s:%s from %s"%(drive_tag, drive[drive_tag]["label"], mount_directory))
+            process_umount=subprocess.Popen(["sudo", "umount", "-R", mount_directory]).wait()
+    except:
+        logger.info("Something went wrong!!!. This is the error:")
+        exc_type, exc_value, exc_traceback=sys.exc_info()
+        logger.error("%s"%(traceback.format_exc()))
+    finally:
+        sock.close()
+        logger.info("Un-mounting %s:%s from %s"%(drive_tag, drive[drive_tag]["label"], mount_directory))
+        process_umount=subprocess.Popen(["sudo", "umount", "-R", mount_directory]).wait()
+        logger.info("Terminated at %s"%(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
