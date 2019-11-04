a1c1efdd2567f794b0d674b79b92f11936a4dca8
nivekg <nivekghazi94@gmail.com>
2019-11-04 12:23:34 +0000

    Now installs everything, starting from a fresh SD Card

--- scripts/install_with_internet.sh
+++ scripts/install_with_internet.sh
@@ -1,26 +1,118 @@
 #!/bin/bash
 
-casperfpga="https://github.com/nivekg/casperfpga.git"
+echo "Moving to home directory"
+cd ~
 
-echo "Downloading casperfpga from "
-echo $casperfpga
+echo "Making direcory software"
+mkdir software
 
-git clone $casperfpga
+echo "Entering software directory"
+cd software
 
-echo "Moving to casperfpga"
+echo "Downloading katcp_devel"
+git clone https://github.com/nivekg/katcp_devel.git
+
+echo "Entering katcp_devel"
+cd katcp_devel
+
+echo "Checking out rpi-devel-casperfpga"
+git checkout rpi-devel-casperfpga
+
+echo "Running: make all"
+make all
+
+echo "Copying ./fpg/kcpfpg to /bin/"
+sudo cp ./fpg/kcpfpg /bin/
+
+echo "Copying ./tcpborphserver3/tcpborphserver3 to /bin/"
+sudo cp ./tcpborphserver3/tcpborphserver3 /bin/
+
+echo "Setting up tcpborphserver3 to run at startup"
+echo "[Unit]
+Description=TCPBorphServer allows programming and communication with the FPGA
+Wants=network.target
+After=syslog.target network-online.target
+
+[Service]
+Type=simple
+ExecStart=/bin/tcpborphserver3 -f -l /dev/null
+Restart=on-failure
+RestartSec=10
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/tcpborphserver3.service
+
+sudo systemctl daemon-reload
+sudo systemctl enable tcpborphserver3
+sudo systemctl start tcpborphserver3
+
+echo "Checking tcpborphserver3 status"
+sudo systemctl status tcpborphserver3
+
+echo "Exiting katcp_devel"
+cd ..
+
+echo "Downloading casperfpga"
+git clone https://github.com/nivekg/casperfpga.git
+
+echo "Entering casperfpga"
 cd casperfpga
-git checkout devel 
 
-echo "Installing casperfpga prerequists"
-pip install -r requirements.txt
+echo "Making casperfpga_dep"
+mkdir ../casperfpga_dep
+
+echo "Downloading casperfpga dependencies"
+pip download -r requirements.txt --no-binary :all: -d ../casperfpga_dep
+
+echo "Installing casperfpga dependencies"
+pip install -r requirements.txt --no-index --find-links ../casperfpga_dep
+
+echo "Switching to devel branch"
+git checkout devel
 
 echo "Installing casperfpga"
 sudo python setup.py install
 
-trimble=https://github.com/mjuenema/python-TSIP.git
+echo "Exiting casperfpga"
+cd ..
+
+echo "Downloading pyaml"
+pip download pyaml --no-binary :all: -d .
+
+echo "Installing pyaml"
+pip install pyaml --no-index --find-link .
+
+echo "Downloading psutil"
+git clone https://github.com/giampaolo/psutil.git
+
+echo "Entering psutil"
+cd psutil
+
+echo "Installing psutil"
+sudo python setup.py install
+
+echo "Exiting psutil"
+cd ..
+
 echo "Downloading python-TSIP"
-git clone $trimble
+git clone https://github.com/mjuenema/python-TSIP.git
+
+echo "Entering python-TSIP"
+cd python-TSIP
 
 echo "Installing python-TSIP"
-cd python-TSIP
 sudo python setup.py install
+
+echo "Exiting python-TSIP"
+cd ..
+
+echo "Exiting software"
+cd ~
+
+echo "Increasing network buffer size to 512Mb"
+bytes=$((512 * 1024000))
+echo "sysctl net.core.rmem_default="$bytes"
+sysctl net.core.rmem_max="$bytes"" | sudo tee -a /etc/sysctl.conf
+
+echo "All done. Please reboot"
